ec3bd1f9ee0a29ed3e5322a8153ff36d5701d02b
Twumasi Augustine <95767645+TwumasiAugustine@users.noreply.github.com>
2022-10-30 16:41:05 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+let myNodeList = document.getElementsByTagName("LI");
+let i;
+for (i = 0; i < myNodeList.length; i++) {
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodeList[i].appendChild(span);
+}
+let close = document.getElementsByClassName("close");
+for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+        let div = this.parentElement;
+        div.style.display = "none";
+    }
+}
+
+let list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+// Create a new list item when clicking on the "Add" button
+function addNewElement() {
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("input").value;
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        document.getElementById("unorderedList").appendChild(li);
+    }
+    document.getElementById("input").value = "";
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            let div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
